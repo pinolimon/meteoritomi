e3066e7ff44e796f3cd143ee4cf834d55f6f570e
pinolimon <137451494+pinolimon@users.noreply.github.com>
2023-07-07 15:20:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ const ancla1 = mindarThree.addAnchor(0);
 const ancla2 = mindarThree.addAnchor(1);
 
 //----- Cargamos los modelos
-cargarModelo("./modelo/modeloMeteorito_lowPoly.glb", ancla1, 0.3);
-cargarModelo("./modelo/modeloMeteorito_lowPoly.glb", ancla2, 0.3);
+cargarModelo("./modelo/modeloMeteorito_lowPoly.glb", ancla1, 0.1);
+cargarModelo("./modelo/modeloMeteorito_lowPoly.glb", ancla2, 0.1);
 
 const start = async () => {
   await mindarThree.start();
